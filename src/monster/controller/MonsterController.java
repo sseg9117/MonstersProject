536df723dcb93111c663a4bf01d6397cac23807a
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2017-09-15 09:26:51 -0600

    added scanner and some more stuff.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,6 +1,8 @@
 package monster.controller;
 
 import monster.model.MarshmallowMonster;
+import java.util.Scanner;
+
 
 public class MonsterController
 {
@@ -10,7 +12,24 @@ public class MonsterController
 		System.out.println(sample);
 		MarshmallowMonster realMonster = new MarshmallowMonster("Jery", 2, 5, 1, true);
 		
+		System.out.println("Sheradon is hungry, so he is going to eat a tentac");
+		realMonster.setTentacleAmount(.5);
+		System.out.println(realMonster);
 		
+		interactWithTheMonster(realMonster);
 		
-	}
+		}
+		
+		private void interactWithTheMonster(MarshmallowMonster currentMonster)
+		{
+			System.out.println(currentMonster.getName() + " wants to know what to eat next");
+			System.out.println(currentMonster.getName() + " suggest arms, they have " + currentMonster.getArmCount());
+			System.out.println("How many do you want to eat?");
+			Scanner myScanner = new Scanner(System.in);
+			int consumed = myScanner.nextInt();
+			currentMonster.setArmCount(currentMonster.getArmCount() - consumed);
+			System.out.println("Ok, now " + currentMonster.getName() + " has " + currentMonster.getArmCount() + "arms left");
+			
+			
+		}
 }
