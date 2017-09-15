b54f228638d0721da5b662777b731e69fecfdf0a
Segota <sseg9117@740s-pl-130.local>
2017-09-15 07:56:11 -0600

    added my controller and runner class to my packages.

---
+++ src/monster/controller/MonsterRunner.java
@@ -0,0 +1,11 @@
+package monster.controller;
+
+public class MonsterRunner
+{
+	public static void main(String [] args)
+	{
+		MonsterController app = new MonsterController();
+		app.start();
+		
+	}
+}
