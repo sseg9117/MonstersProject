029e8bba2c11e4f49264f182f18361bf671510bc
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2017-09-19 09:33:34 -0600

    added a display class

---
+++ src/monter/view/MonsterDisplay.java
@@ -0,0 +1,22 @@
+package monter.view;
+
+import javax.swing.JOptionPane;
+
+public class MonsterDisplay
+{
+	
+	public void displayxt(String textToDisplay)
+	{
+		JOptionPane.showMessageDialog(null, textToDisplay);
+	}
+	
+	public String getResponse(String textToDisplay)
+	
+	{
+		String response = "";
+		
+		response += JOptionPane.showInputDialog(null, textToDisplay);
+		
+		return response;
+	}
+}
