bbc352b1b66960e58e8c4dcd9fc8409b001c4ddd
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2017-09-19 09:08:23 -0600

    added else if and else statement

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -10,26 +10,64 @@ public class MonsterController
 	{
 		MarshmallowMonster sample = new MarshmallowMonster();
 		System.out.println(sample);
-		MarshmallowMonster realMonster = new MarshmallowMonster("Jery", 2, 5, 1, true);
-		
+		MarshmallowMonster realMonster = new MarshmallowMonster("Jery", 10, 10, 10, true);
+	
 		System.out.println("Sheradon is hungry, so he is going to eat a tentac");
-		realMonster.setTentacleAmount(.5);
+		realMonster.setTentacleAmount(10);
 		System.out.println(realMonster);
 		
 		interactWithTheMonster(realMonster);
-		
-		}
+	}
 		
 		private void interactWithTheMonster(MarshmallowMonster currentMonster)
+
 		{
 			System.out.println(currentMonster.getName() + " wants to know what to eat next");
 			System.out.println(currentMonster.getName() + " suggest arms, they have " + currentMonster.getArmCount());
 			System.out.println("How many do you want to eat?");
 			Scanner myScanner = new Scanner(System.in);
 			int consumed = myScanner.nextInt();
-			currentMonster.setArmCount(currentMonster.getArmCount() - consumed);
-			System.out.println("Ok, now " + currentMonster.getName() + " has " + currentMonster.getArmCount() + "arms left");
-			
-			
+				if(consumed < 0)
+				{
+					System.out.println("You cannot eat a negative amount silly human");		
+				}
+				else if(consumed == 0)
+				{
+					System.out.println("Not that hungry are you?");
+				}
+				else if(consumed > currentMonster.getArmCount())
+				{
+					System.out.println("That is impossible - I only have " + currentMonster.getArmCount() + "arms!!!");
+				}
+				else
+				{
+					currentMonster.setArmCount(currentMonster.getArmCount() - consumed);
+					System.out.println("Thank you so much! I only have this many arms now " + currentMonster.getArmCount());
+				}	
+					System.out.println("How many tentacles do you want then? I have " + currentMonster.getTentacleAmount());
+					Scanner myScanner2 = new Scanner(System.in);
+					int consumes = myScanner2.nextInt();
+								
+				if(consumes > 10)
+				{
+					System.out.println("You cant eat more than 10 loser");
+				}
+				else if(consumes == 0)
+				{
+					System.out.println("you are not hungry i see");
+				}
+				else if(consumes > currentMonster.getTentacleAmount())
+				{
+					System.out.println(" I have " + currentMonster.getTentacleAmount());
+				}
+				else
+				{
+					currentMonster.setTentacleAmount(currentMonster.getTentacleAmount() - consumes );
+					System.out.println("Thank you so much! I only have this many tentacles now " + currentMonster.getTentacleAmount());
+				}
+					
 		}
 }
+				
+					
+			
